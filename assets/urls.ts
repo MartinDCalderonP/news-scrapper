0b7ecd95da49292667e3d0f146b0cfa5929a6ff3
Martín <martincalderon92@outlook.com>
2025-01-12 23:18:44 -0300

    feat: add predefined article URLs for scraping

---
+++ assets/urls.ts
@@ -0,0 +1,16 @@
+const urls = [
+  'https://anccom.sociales.uba.ar/2024/04/22/los-trabajadores-de-telam-presentan-su-propio-proyecto-legislativo/',
+  'https://anccom.sociales.uba.ar/2024/05/19/y-el-polideportivo-donde-esta/',
+  'https://anccom.sociales.uba.ar/2024/05/25/la-salud-tambien-para/',
+  'https://anccom.sociales.uba.ar/2024/06/07/los-arboles-son-necesarios-para-vivir-en-la-ciudad/',
+  'https://anccom.sociales.uba.ar/2024/07/09/los-mismos-conflictos-estan-sucediendo-en-todos-los-barrios/',
+  'https://anccom.sociales.uba.ar/2024/08/07/bangladesh-se-queda-sin-primera-ministra-tras-las-protestas-juveniles/',
+  'https://anccom.sociales.uba.ar/2024/08/22/denunciar-los-crimenes-de-guerra-de-israel-no-es-antisemitismo/',
+  'https://anccom.sociales.uba.ar/2024/09/02/los-trabajadores-de-prensa-reclaman-un-salario-por-arriba-de-la-linea-de-pobreza/',
+  'https://anccom.sociales.uba.ar/2024/09/12/la-musica-se-paga-o-se-apaga/',
+  'https://anccom.sociales.uba.ar/2024/12/04/libertad-de-expresion-en-la-era-digital/',
+  'https://anccom.sociales.uba.ar/2024/11/22/el-derecho-a-reparar/',
+  'https://anccom.sociales.uba.ar/2024/12/17/ciudad-universitaria-se-convirtio-en-disney/'
+]
+
+export default urls
